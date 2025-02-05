f9f01531bda46dd4916c79576b02d4c663e42a79
fhy <wdsj002@126.com>
2025-02-05 15:17:27 +0800

    注册验证码

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,31 +1,39 @@
 <!DOCTYPE html>
-<html lang="{{str_replace('_','-',app()->getLocale())}}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title','Larabbs')</title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    {{-- scrf token --}}
-    <meta name='scrf-token' content="{{ csrf_token() }}">
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <link rel="stylesheet" href="{{mix('css/app.css')}}">
+  <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
+
+  <!-- Styles -->
+  <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+
 </head>
+
 <body>
-    <div id=app class="{{route_class()}}-page">
-        @include('layouts._header')
+  <div id="app" class="{{ route_class() }}-page">
 
-        <div class="container">
+    @include('layouts._header')
 
-            @include('shared._message')
+    <div class="container">
 
-            @yield('content')
+      @include('shared._messages')
 
-        </div>
+      @yield('content')
 
-        @include('layouts._footer')
     </div>
-    {{-- script --}}
-    <script src="{{mix('js/app.js')}}"></script>
+
+    @include('layouts._footer')
+  </div>
+
+  <!-- Scripts -->
+  <script src="{{ mix('js/app.js') }}"></script>
 </body>
+
 </html>
