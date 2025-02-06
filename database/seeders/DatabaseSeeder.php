93a6c405108066943aac6d8b926375ea6f6188e8
fhy <wdsj002@126.com>
2025-02-06 17:35:42 +0800

    回复数据

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call(UsersTableSeeder::class);
+		$this->call(RepliesTableSeeder::class);
         $this->call(TopicsTableSeeder::class);
     }
 }
