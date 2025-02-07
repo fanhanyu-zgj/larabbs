9d518a19dc50c88d6e46aa4f29d208d7dc0083e8
fhy <wdsj002@126.com>
2025-02-07 16:31:34 +0800

    首页话题列表

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-Route::get('/', 'PageController@root')->name('root');
+Route::get('/', 'TopicsController@index')->name('root');
 
 //Auth::routes();
 
